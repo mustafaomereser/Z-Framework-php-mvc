21fec33784af263576f205da7dde0d6a5a498738
Mustafa mer ESER <mustafaomereser@gmail.com>
2022-06-11 22:06:50 +0300

    update welcome template

--- resource/views/welcome.php
+++ resource/views/welcome.php
@@ -9,7 +9,7 @@ use Core\Facedas\Lang;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title><?= Lang::get('lang.welcome') ?></title>
 
     <style>
         body {
