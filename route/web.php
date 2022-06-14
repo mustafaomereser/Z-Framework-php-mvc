e2895f70c5f6b118cacd6b292dbbd846c449ce91
Mustafa mer ESER <mustafaomereser@gmail.com>
2022-06-14 15:44:11 +0300

    Commit

--- route/web.php
+++ route/web.php
@@ -1,20 +1,10 @@
 <?php
 
-use Core\Facedas\Alerts;
 use Core\Facedas\Lang;
-use Core\Helpers\File;
 use Core\Route;
 use Core\View;
 
 Route::any('/', function () {
-
-    $upload = File::upload('/uploads', $_FILES['file']);
-    if($upload) Alerts::success('Dosya yüklendi.');
-
-    $resize = File::resizeImage($upload, 100, 100);
-
-    if($resize) Alerts::success('Resim boyutlandırıldı.');
-
     return View::view('welcome');
 });
 
