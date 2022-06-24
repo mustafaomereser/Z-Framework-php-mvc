57db2c8273dd0ec77488331783147a720a43f334
Mustafa mer ESER <mustafaomereser@gmail.com>
2022-06-24 16:46:37 +0300

    Add Helpers/Date Class

---
+++ core/Helpers/Date.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Core\Helpers;
+
+class Date
+{
+    public static function setLocale($set)
+    {
+        return date_default_timezone_set($set);
+    }
+
+    public static function locale()
+    {
+        return date_default_timezone_get();
+    }
+
+    public static function format($date, $format = 'd.m.Y')
+    {
+        return date($format, (is_string($date) ? strtotime($date) : $date));
+    }
+
+    public static function now()
+    {
+        return date('d.m.Y H:i');
+    }
+
+    public static function timestamp()
+    {
+        return date('Y-m-d H:i:s');
+    }
+}
