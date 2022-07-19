f589351372aea4a0a7b081228c866eb48548d462
Mustafa mer ESER <mustafaomereser@gmail.com>
2022-07-19 16:08:18 +0300

    added automatic update for timestamps

--- route/web.php
+++ route/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Models\User;
-use zFramework\Core\Crypter;
-use zFramework\Core\Facades\Str;
 use zFramework\Core\Route;
 
 // use App\Controllers\HomeController;
@@ -11,17 +8,6 @@ use zFramework\Core\Route;
 Route::get('/', 'HomeController@index');
 
 Route::get('/test', function () {
-    $user = new User;
-
-    // $user->insert([
-    //     'username'  => Str::rand(20),
-    //     'password'  => Crypter::encode('test'),
-    //     'email'     => Str::rand(5) . "@" . Str::rand(4) . ".com",
-    //     'api_token' => Str::rand(60)
-    // ]);
-
-    $user->delete();
-
     return view('test');
 })->name('test');
 
