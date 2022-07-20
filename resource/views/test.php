34ff466c5c38a16620b0ca5882a302a437fe6fd9
Mustafa mer ESER <mustafaomereser@gmail.com>
2022-07-20 16:35:12 +0300

    Commit

--- resource/views/test.php
+++ resource/views/test.php
@@ -6,4 +6,7 @@ Users From Provider:
 User From WS:
 <pre>
 <?php print_r($user) ?>
+
+if you seen: Undefined variable: user, go to `<a href="<?= route('ws.test', ['id' => 1]) ?>"><?= route('ws.test', ['id' => 1]) ?></a>`.
+if you still seen nothing add a user to database.
 </pre>
--- ws/routes.php
+++ ws/routes.php
@@ -5,14 +5,14 @@ use zFramework\Core\Route;
 use zFramework\Core\Ws;
 
 
-Route::ws('get', '/test2', function () {
+Route::ws('get', '/test', function () {
     return Ws::send('test', null, function ($data) {
         echo $data;
     });
 });
 
-Route::ws('get', '/test2/{id}', function ($id) {
+Route::ws('get', '/test/{id}', function ($id) {
     return Ws::send('user', "id=$id", function ($data) {
         return view('test', ['user' => json_decode($data, true)]);
     });
-});
+})->name('ws.test');
