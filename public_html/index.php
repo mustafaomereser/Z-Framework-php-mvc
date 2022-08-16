955a18a70a3240c40c176b43023b9f6aa6e00998
Mustafa mer ESER <mustafaomereser@gmail.com>
2022-08-16 17:03:48 +0300

    Added https option public_html/index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,7 @@
 <?php
+// for https redirect 
+# if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") die(header('Location: https://' . ($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])));
+
 // Set base path
 define('BASE_PATH', dirname(__DIR__));
 define('FRAMEWORK_PATH', BASE_PATH . "/zFramework");
